cc0f69b72896233f6ed93ed14b9cc8b5eff516da
Alexis V <definealexis@gmail.com>
2022-03-09 17:41:37 -0600

    m1

---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container py-5">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <a href="" class="btn btn-primary float-end btn-sm">Add student</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
